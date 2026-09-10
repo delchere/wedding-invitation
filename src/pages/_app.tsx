2d8c44e7c3f21f8ccba81458fda8140696dd359f
juniorGitH <emmanuel.amela@studentambassadors.com>
2026-09-10 21:40:43 +0000

    Remove login screen from wedding site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
 import type { AppContext, AppInitialProps, AppProps } from 'next/app'
@@ -8,8 +8,6 @@ import 'slick-carousel/slick/slick.css'
 import '@/styles/globals.css'
 import '@/styles/react-slick.css'
 import { Splash } from '@/components/splash'
-import { userHasPrevLogin } from '@/utils/password'
-import { LoginPage } from '@/components/login'
 import weddingConfig from '@/config/wedding.config'
 import { LocaleProvider } from '@/context/locale-context'
 
@@ -18,13 +16,6 @@ type CustomAppProps = {}
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function MyCustomApp({ Component, pageProps }: AppProps & CustomAppProps) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  useEffect(() => {
-    const hasPrevLogin = userHasPrevLogin()
-    setIsLoggedIn(hasPrevLogin)
-  }, [])
-
   return (
     <>
       <Head>
@@ -39,7 +30,8 @@ export function MyCustomApp({ Component, pageProps }: AppProps & CustomAppProps)
       <MUIProvider>
         <CssBaseline />
         <LocaleProvider>
-          {isLoggedIn ? <><Splash timeout={200} /><Component {...pageProps} /></> : <><Splash timeout={200} /><LoginPage /></>}
+          <Splash timeout={200} />
+          <Component {...pageProps} />
         </LocaleProvider>
       </MUIProvider>
     </>
