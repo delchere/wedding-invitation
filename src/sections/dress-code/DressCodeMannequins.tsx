452bf02331987d24847bc218dafadeaf4062953d
EKLOU Lauryne Victoria <lv.eklou2@gmail.com>
2026-09-21 00:03:46 +0000

    fix: use img tag for static export on GitHub Pages

--- src/sections/dress-code/DressCodeMannequins.tsx
+++ src/sections/dress-code/DressCodeMannequins.tsx
@@ -1,17 +1,16 @@
 import React, { FC } from 'react'
-import Image from 'next/image'
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 const DressCodeMannequins: FC = () => (
   <div className="dress-code-figures">
-    <Image
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img
       src={`${basePath}/images/dress-code/mannequins.png`}
       alt=""
       width={427}
       height={585}
       className="dress-code-figures__img"
-      priority={false}
       aria-hidden="true"
     />
   </div>
